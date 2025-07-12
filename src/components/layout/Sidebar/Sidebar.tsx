01eb1521996ebc9b6837f23c54d43cb0ef409053
Nguyen Ba Long <longnguyen132005@gmail.com>
2025-07-12 23:35:20 +0700

    t

--- src/components/layout/Sidebar/Sidebar.tsx
+++ src/components/layout/Sidebar/Sidebar.tsx
@@ -87,6 +87,11 @@ export const Sidebar: React.FC<SidebarProps> = ({ className = '', onOpenPanel })
       path: '/payment',
     },
     {
+      icon: <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path d="M10 2a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V4a2 2 0 00-2-2h-2zm-6 8a2 2 0 012-2h12a2 2 0 012 2v6a2 2 0 01-2 2H6a2 2 0 01-2-2v-6zm2 0v6h12v-6H6z" /></svg>,
+      label: 'Quản lý quy định',
+      path: '/regulations',
+    },
+    {
       icon: <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path d="M9 2a1 1 0 00-1 1v2.382a1 1 0 01-.553.894l-5.447 2.724A1 1 0 002 10.618V17a1 1 0 001 1h14a1 1 0 001-1v-6.382a1 1 0 00-.553-.894l-5.447-2.724A1 1 0 0112 5.382V3a1 1 0 00-1-1H9zm1 4.618l5 2.5V16H5v-6.882l5-2.5z" /></svg>,
       label: 'Lập báo cáo',
       path: '/reports',
